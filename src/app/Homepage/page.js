08f63b447b75963a9390f6af07af765d9c62e5da
1kodexxx <pvntheraxxx@gmail.com>
2025-10-21 07:08:36 +0300

    header

--- src/app/Homepage/page.js
+++ src/app/Homepage/page.js
@@ -1,4 +1,10 @@
-// app/Homepage/page.tsx
+import Header from "../components/Header";
+
 export default function Page() {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col min-h-screen bg-background text-foreground">
+      <Header />
+      <main className="flex flex-1 pt-16"></main>
+    </div>
+  );
 }
